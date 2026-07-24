6940c0abe5b7d9bb671f3c9b0ccfbf681c4977c3
rank1984 <rksoccertraning@gmail.com>
2026-07-24 17:04:04 +0300

    Update __init__.py

--- trade_manager/__init__.py
+++ trade_manager/__init__.py
@@ -1,1 +1,6 @@
 from .trade_manager import TradeManager
+class PaperTrader:
+    def __init__(self, paper: bool = True):
+        base_url = 'https://paper-api.alpaca.markets' if paper else 'https://api.alpaca.markets'
+        self.api = tradeapi.REST(ALPACA_API_KEY, ALPACA_SECRET_KEY, base_url=base_url)
+        # ... שאר הקוד ...
